e3a429deed211ba9bf485fa93511bb34fc13929c
JoãoPauloDonatoJ <joaopaulodonato91@gmail.com>
2026-05-31 22:52:04 -0300

    feat: initial project structure with ef core and user migration

---
+++ ProjectMannager.API/Data/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectMannager.API.Entities;
+
+namespace ProjectMannager.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email).IsUnique();
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.UserName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.PasswordHash)
+                .IsRequired()
+                .HasMaxLength(255);
+
+        }
+    }
+}
---
+++ ProjectMannager.API/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectMannager.API.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
+        public string UserName  { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O email deve ser um endereço de email válido.")]
+        [StringLength(150, ErrorMessage = "O email deve ter no máximo 150 caracteres.")]
+        public string Email  { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        public string PasswordHash  { get; set; }
+
+        public DateTime CreatedAt  { get; set; } = DateTime.Now;
+    }
+}
