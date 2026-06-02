238eb540fb2461e6debea9f7a96a77ca22cf0355
JoãoPauloDonatoJ <joaopaulodonato91@gmail.com>
2026-06-01 22:18:05 -0300

    feat: finalize launch settings, cors policy and jwt infrastructure

--- ProjectMannager.API/Program.cs
+++ ProjectMannager.API/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using ProjectMannager.API.Data;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +14,47 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVite", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") //URL do Frontend Vite
+               .AllowAnyHeader()
+               .WithMethods("GET", "POST", "PUT", "DELETE");
+    });
+});
+
+// Captura a chave do appsettings
+var jwtSecret = builder.Configuration["Jwt:Secret"];
+var key = Encoding.UTF8.GetBytes(jwtSecret);
+
+builder.Services.AddAuthentication(options =>
+{
+    // Define que o padrão de autenticação da API será via Bearer Token (JWT)
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false; // Como estamos local, podemos deixar false
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,   // Podemos evoluir isso depois
+        ValidateAudience = false, // Podemos evoluir isso depois
+        ClockSkew = TimeSpan.Zero // Zera o tempo de tolerância padrão para expiração do token
+    };
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseCors("AllowVite");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
