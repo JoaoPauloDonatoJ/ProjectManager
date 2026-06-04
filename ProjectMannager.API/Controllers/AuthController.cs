ae0a6c5af171b11b2edfd3ebe04569f3fafbe628
JoãoPauloDonatoJ <joaopaulodonato91@gmail.com>
2026-06-04 09:22:19 -0300

    feat: implement user registration and cryptographically secure jwt login authentication

---
+++ ProjectMannager.API/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectMannager.API.Services;
+using ProjectMannager.API.DTOs;
+using ProjectMannager.API.Entities;
+using ProjectMannager.API.Data;
+using BCrypt.Net;
+
+namespace ProjectMannager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public readonly AppDbContext _context;
+        public readonly ITokenService _tokenService;
+
+        public AuthController(AppDbContext context, ITokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            if(await _context.Users.AnyAsync(u => u.Email == registerDto.Email))
+                return BadRequest("Email já se encontra em uso !");
+
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(registerDto.Password);
+
+            var user = new User
+            {
+                UserName = registerDto.Username,
+                Email = registerDto.Email,
+                PasswordHash = passwordHash
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok( new {message = "Usuário registrado com sucesso !"});
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == loginDto.Email);
+
+            if(user == null || !BCrypt.Net.BCrypt.Verify(loginDto.Password, user.PasswordHash))
+            {
+                return Unauthorized("Email ou senha inválidos !");
+            }
+
+            var token = _tokenService.GenerateToken(user);
+
+            return Ok(new { token });
+        }
+    }
+}
---
+++ ProjectMannager.API/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using ProjectMannager.API.Entities;
+
+namespace ProjectMannager.API.Services
+{
+    public interface ITokenService
+    {
+        string GenerateToken(User user);
+    }
+}
